b2595673198d51548129b952bbc5fb64861cb13a
Kholkin <kholkins@mail.ru>
2020-04-09 16:08:24 +0300

    new

--- app/src/main/java/Utils/Util.java
+++ app/src/main/java/Utils/Util.java
@@ -4,7 +4,7 @@ public class Util {
 
     public static final int DATABASE_VERSION = 1;
     public static final String DATABASE_NAME = "carsDB";
-    public static final String TABLE_NAME = "cars";
+//    public static final String TABLE_NAME = "cars";
 
 //    public static final String KEY_ID = "id";
 //    public static final String KEY_NAME = "name";
